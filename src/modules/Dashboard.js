37d1ab82ac1a39fb43faac12b73c16398a33fdc1
NunoXavier <insaneburn@gmail.com>
2021-02-06 00:32:59 +0000

    Updates

--- src/modules/Dashboard.js
+++ src/modules/Dashboard.js
@@ -1,6 +1,9 @@
 import WarningLights from './WarningLights';
 import Speedometer from './Speedometer';
 import Tachometer from './Tachometer';
+import Odometer from './Odometer';
+import WaterTemperature from './WaterTemperature';
+import FuelLevel from './FuelLevel';
 
 import './Dashboard.scss';
 
@@ -10,7 +13,9 @@ function Dashboard() {
             <WarningLights/>
             <div className="MiddleModule row justify-content-center">
                 <div className="col-4">
-                    odometer / water tmp / fuel lvl
+                    <Odometer />
+                    <WaterTemperature />
+                    <FuelLevel />
                 </div>
                 <div className="col-8">
                     <Speedometer/>
---
+++ src/modules/Odometer/index.js
@@ -0,0 +1,14 @@
+import './index.scss';
+
+function Odometer() {
+
+    let totalKms = 990.2;
+
+    return (
+        <div className="Odometer mb-3">
+            // TODO odometer
+        </div>
+    );
+}
+
+export default Odometer;
